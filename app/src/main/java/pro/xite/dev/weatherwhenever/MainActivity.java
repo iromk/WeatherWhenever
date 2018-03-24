99c12872b70f1c6aac0abc481ab5d3705f8b5904
nnm <roman.syrchin@gmail.com>
2018-03-24 15:46:03 +0500

    ADD Random forecast and tips on click.

--- app/src/main/java/pro/xite/dev/weatherwhenever/MainActivity.java
+++ app/src/main/java/pro/xite/dev/weatherwhenever/MainActivity.java
@@ -6,14 +6,30 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, Spinner.OnItemSelectedListener {
+
+    private Spinner spinnerCityList;
+    private String[] forecasts;
+    private String[] tipsOfTheDay;
+    private int totalForecasts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spinnerCityList = findViewById(R.id.spinner_city_list);
+        spinnerCityList.setOnItemSelectedListener(this);
+        forecasts = getResources().getStringArray(R.array.forecasts_hardcoded_list);
+        tipsOfTheDay = getResources().getStringArray(R.array.todo_actions_list);
+        totalForecasts = forecasts.length;
+
+        // implementation assumes that each forecast has certain related tip.
+        if (totalForecasts != tipsOfTheDay.length) throw new AssertionError();
+
         loadSelectionFromPreferences();
     }
 
@@ -23,6 +39,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         if(id == R.id.button_get_forecast) {
             Log.d("TRACER", "The button has been clicked");
             saveSelectionInPreferences();
+            makeUsualWeatherForecast();
         }
     }
 
@@ -31,12 +48,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         final SharedPreferences sharedPref = context.getSharedPreferences(
                 getString(R.string.preference_file_key), Context.MODE_PRIVATE);
         final SharedPreferences.Editor editor = sharedPref.edit();
-        final Spinner spinner = findViewById(R.id.spinner_city_list);
-        final int last_city_index = spinner.getSelectedItemPosition();
+        final int last_city_index = spinnerCityList.getSelectedItemPosition();
         editor.putInt(getString(R.string.selected_city_index_prefs_key), last_city_index);
         editor.apply(); // tip! writes in a background
         //editor.commit(); // tip! writes data immediately
     }
+
+
 
     private void loadSelectionFromPreferences() {
         final Context context = this;
@@ -44,7 +62,32 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 getString(R.string.preference_file_key), Context.MODE_PRIVATE);
         final int default_city_index = 0; //getResources().getStringArray(R.array.city_list)[0];
         final int last_city_index = sharedPref.getInt(getString(R.string.selected_city_index_prefs_key), default_city_index);
-        final Spinner spinner = findViewById(R.id.spinner_city_list);
-        spinner.setSelection(last_city_index,true);
+        spinnerCityList.setSelection(last_city_index,true);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        Log.d("TRACER", spinnerCityList.getSelectedItem().toString());
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    private void makeUsualWeatherForecast() {
+        final int randomWeather = (int) (Math.random() * totalForecasts);
+        setForecastText(forecasts[randomWeather]);
+        setTextDailyTip(tipsOfTheDay[randomWeather]);
+    }
+
+    private void setTextDailyTip(String text) {
+        TextView tv = findViewById(R.id.textview_tipoftheday);
+        tv.setText(text);
+    }
+
+    private void setForecastText(String text) {
+        TextView tv = findViewById(R.id.textview_forecast);
+        tv.setText(text);
     }
 }
