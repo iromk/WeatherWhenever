57c4673dafc86af0336ef80bb35ab861db1523fb
nnm <roman.syrchin@gmail.com>
2018-03-29 18:53:10 +0500

    UPD button style changed to circle. UPD weather request on click for city typed in the EditText.

--- app/src/main/java/pro/xite/dev/weatherwhenever/MainActivity.java
+++ app/src/main/java/pro/xite/dev/weatherwhenever/MainActivity.java
@@ -8,11 +8,14 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener, Spinner.OnItemSelectedListener {
+import pro.xite.dev.weatherwhenever.owm.OWMActualWeatherProvider;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener, Spinner.OnItemSelectedListener, ViewUpdatable {
 
     public static final String LATEST_FORECAST_KEY = "latest_forecast";
     public static final String FRIENDS_RESPONSE = "friends_response";
@@ -25,15 +28,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        spinnerCityList = findViewById(R.id.spinner_city_list);
-        spinnerCityList.setOnItemSelectedListener(this);
+//        spinnerCityList = findViewById(R.id.spinner_city_list);
+//        spinnerCityList.setOnItemSelectedListener(this);
         ForecastProvider.create(this);
-        loadSelectionFromPreferences();
+//        loadSelectionFromPreferences();
 
-        if(savedInstanceState != null) {
-            reliableForecast = (Forecast) savedInstanceState.getSerializable(LATEST_FORECAST_KEY);
+//        if(savedInstanceState != null) {
+//            reliableForecast = (Forecast) savedInstanceState.getSerializable(LATEST_FORECAST_KEY);
 //            setForecastText(reliableForecast.getWeather());
-        }
+//        }
     }
 
     @Override
@@ -47,17 +50,19 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         int id = view.getId();
         if(id == R.id.button_get_forecast) {
             Log.d(TAG_TRACER, "The button has been clicked");
-            reliableForecast = ForecastProvider.makeReliableForecast(getSelectedCityName());
+            EditText editText = findViewById(R.id.edittext_cityname);
+            new OWMActualWeatherProvider().request(editText.getText().toString(), this);
+//            reliableForecast = ForecastProvider.makeReliableForecast(getSelectedCityName());
 //            setForecastText(reliableForecast.getWeather());
-            Intent intent = new Intent(this, ForecastActivity.class);
-            intent.putExtra(ForecastActivity.FORECAST_OBJECT, reliableForecast);
-            startActivityForResult(intent, 1);
+//            Intent intent = new Intent(this, ForecastActivity.class);
+//            intent.putExtra(ForecastActivity.FORECAST_OBJECT, reliableForecast);
+//            startActivityForResult(intent, 1);
         }
     }
 
     @Override
     protected void onPause() {
-        saveSelectionInPreferences();
+//        saveSelectionInPreferences();
         super.onPause();
     }
 
@@ -97,12 +102,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     // TODO move to external class
     private void loadSelectionFromPreferences() {
-        final Context context = this;
-        final SharedPreferences sharedPref = context.getSharedPreferences(
-                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
-        final int default_city_index = 0; //getResources().getStringArray(R.array.city_list)[0];
-        final int last_city_index = sharedPref.getInt(getString(R.string.selected_city_index_prefs_key), default_city_index);
-        spinnerCityList.setSelection(last_city_index,true);
+//        final Context context = this;
+//        final SharedPreferences sharedPref = context.getSharedPreferences(
+//                getString(R.string.preference_file_key), Context.MODE_PRIVATE);
+//        final int default_city_index = 0; //getResources().getStringArray(R.array.city_list)[0];
+//        final int last_city_index = sharedPref.getInt(getString(R.string.selected_city_index_prefs_key), default_city_index);
+//        spinnerCityList.setSelection(last_city_index,true);
     }
 
     @Override
@@ -118,5 +123,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void setForecastText(String text) {
         TextView tv = findViewById(R.id.textview_forecast);
         tv.setText(text);
+    }
+
+    @Override
+    public <T> void updateViews(T owm) {
+        Log.d(TAG_TRACER, Helpers.getMethodName());
     }
 }
