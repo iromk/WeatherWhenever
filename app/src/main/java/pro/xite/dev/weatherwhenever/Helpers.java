08ca49964838849a3d30746fd7dcaa856f697b19
nnm <roman.syrchin@gmail.com>
2018-04-15 10:31:22 +0500

    FIX global warming were hardcoded ))

--- app/src/main/java/pro/xite/dev/weatherwhenever/Helpers.java
+++ app/src/main/java/pro/xite/dev/weatherwhenever/Helpers.java
@@ -36,8 +36,8 @@ public class Helpers {
     public static String tempToString(float tFloat) {
         final int tInt = (int)Math.abs(tFloat);
         String tPrefix = " ";
-        if(tInt > 0 ) tPrefix = "+";
-        if(tInt < 0 ) tPrefix = "-";
+        if(tFloat > 0 ) tPrefix = "+";
+        if(tFloat < 0 ) tPrefix = "-";
         final String tIntStr = String.valueOf(tInt);
         return String.format("%s%s", tPrefix, tIntStr);
     }
